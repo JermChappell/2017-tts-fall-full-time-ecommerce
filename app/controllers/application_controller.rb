969cc0f7019f03bc4cca729db97438f248ec2f22
Jeremy Chappell <jeremy.chappell87@gmail.com>
2017-10-16 14:35:29 +0000

    cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before{action :authenticate_user!, except: [show]
+  before_action :configure_permittex_paramaters, if: :devise_controller?
+  before_action :Categories, :brands
+  
+  protected 
+  
+  
+  
+  def configure_permitted_parameters
+      devise_parameters_sanatizer.permit(:sign_up, keys: [:role])
+      devise_parameters_sanatizer.permit(:account_update, keys: [:role])
+  end
 end
